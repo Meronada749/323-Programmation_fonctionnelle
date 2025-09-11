c733539eca0b63a01a4e64880685bf9ad0dfc52b
Meronada749 <meron.essayas@eduvaud.ch>
2025-09-11 12:14:42 +0200

    Push

---
+++ exoMeron/words/Person.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+public class Person
+{
+    // Constructor
+    public Person(string name, int age)
+    {
+        Name = name;
+        Age = age;
+    }
+
+    // Auto-properties
+    public string Name { get; set; }
+    public int Age { get; set; }
+
+}

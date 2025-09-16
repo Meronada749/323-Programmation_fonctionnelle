f2b9280c93f2e35c3435a9906270a622afb4aaa1
Meronada749 <meron.essayas@eduvaud.ch>
2025-09-16 18:10:19 +0200

    chore(word):Finished words

--- exoMeron/words/Person.cs
+++ exoMeron/words/Person.cs
@@ -1,6 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-//Console.WriteLine("Hello, World!");
-
+﻿
 public class Person
 {
     // Constructor
--- exoMeron/words/Program.cs
+++ exoMeron/words/Program.cs
@@ -1,211 +1,117 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+string[] words = { "bonjour", "hello", "monde", "vert", "rouge", "blex", "jaune"}; //array of strings
 
-//string[] words = { "bonjour", "hello", "monde", "vert", "rouge", "bleu", "jaune" };
-
-//var charValues = new Dictionary<char, double>();
-
-//charValues.Add('a', 1.5);
-
-//double Epsilon(string word)
-//{
-//    return word.Length;
-//}
-
-
-//Console.WriteLine(Epsilon("meron"));
-//Console.WriteLine(charValues['a']);
-
-
-//string[] words = { "bonjour", "hello", "monde", "vert", "rouge", "bleux", "jaune" };
-
-//IEnumerable<string> query = from word in words
-//                            where word.Length == 4
-//                            where !word.Contains("x")
-//                            where word.Average(w => w.Length);
-//                            select word;
-
-//foreach (string str in query)
-//{
-//    Console.WriteLine(str);
-//}
-
-/* This code produces the following output:
-
-    the
-    fox
-*/
-
-//string[] words = { "bonjour", "hello", "monde", "vert", "rouge", "bleu", "jaune" };
-
-//Func<string, bool> noX = w => !w.Contains('x');
-
-//Func<string, bool> fourCharsOrMore = w => w.Length >= 4;
-
-//double avgLength = words.Average(w => w.Length);
-
-//Func<string, bool> sameasAverage = w => w.Length == avgLength;
-
-//Console.WriteLine(avgLength);
-
-//Console.WriteLine(string.Join(", ",words.Where(noX).Where(fourCharsOrMore).Where(longerThanAverage)));
-
-//foreach (string item in words.Where(noX).Where(fourCharsOrMore).Where(longerThanAverage))
-//{
-//    Console.WriteLine(item);
-//}
-
-
-string[] words = { "bonjour", "hello", "monde", "vert", "rouge", "bleu", "jaune" };
-
-Func<string, bool> noX = w => !w.Contains('x');
-
-//Console.WriteLine(noX("Meronex")); //False
-
-//Console.WriteLine(words.All(noX)); //True
-
-Console.WriteLine(string.Join(',' , words.Where(noX))); //Print all that does not have x
-
-//Console.WriteLine("");
-
-//bool Nox(string w)
+//bool noX(string w)
 //{
 //    return !w.Contains('x');
 //}
 
-//Console.WriteLine(Nox("Meronex")); //False
-//Console.WriteLine(words.All(Nox)); //True
+Func<string, bool> noX = w => !w.Contains('x');
 
-
-Console.WriteLine("");
-
-List<Person> people = new() {
-    new Person ( name: "John", age: 25 ),
-    new Person ( name: "Mary", age: 30 ),
-    new Person ( name: "Jane", age: 20 ),
-    new Person ( name: "Bob", age: 35 )
-};
-
-List<Person> adults = people.Where(p => p.Age > 18).ToList();
-
-//adults.ForEach(n => Console.WriteLine(n.Name + " " + n.Age));
-
-Console.WriteLine("");
-
-Console.WriteLine(string.Join(", ", adults.Select(n => n.Name + " " + n.Age)));
-
-Console.WriteLine(string.Join(", ", adults.Where(adults)));
-
-
-//Console.WriteLine(String.Join(',', people.Where(noX)));
-
-
-
-//List<Persons> peoples = new() {
-//    new Persons() { Name = "John", Age = 25 }
-//};
-
-//public class Persons
-//{
-//    public string Name { get; set; }
-//    public int Age { get; set; }
-
-//}
-
-
-
-
-
-/*
 Func<string, bool> fourCharsOrMore = w => w.Length >= 4;
 
-double avgLength = words.Average(w => w.Length);
+//double avgLength = words.Average(w => w.Length); //average length as double
+
+int avgLength = (int)words.Average(w => w.Length); //casting to int will round down the average length
 
 Func<string, bool> sameasAverage = w => w.Length == avgLength;
 
-Console.WriteLine("Choose an option:");
-Console.WriteLine("0: Print all words with 4 characters or more");
-Console.WriteLine("1: Print all words that don't contain 'x'");
-Console.WriteLine("2: Print average word length and words that have as many characters as the average number of characters in the list");
+//Console.WriteLine("The average length is: " + avgLength); //Print average length of words
+//Console.WriteLine("Don't have x: " + string.Join(", ",words.Where(noX))); //Print all that does not have x
+//Console.WriteLine("Has 4 or more chars: " + string.Join(", ",words.Where(fourCharsOrMore))); //Print all that has 4 or more characters
+//Console.WriteLine("Has the same chars as avg: " + string.Join(", ",words.Where(sameasAverage))); //Print all that has same number of characters as the average
 
-string choice = Console.ReadLine();
+//Console.WriteLine("Don't have x: " +
+//    string.Join(", ", words.Where(noX).Select(w => $"'{w}'")));
 
+//Console.WriteLine("Has 4 or more chars: " +
+//    string.Join(", ", words.Where(fourCharsOrMore).Select(w => w)));
 
+//Console.WriteLine("Has the same chars as avg: " +
+//    string.Join(", ", words.Where(sameasAverage).Select(w => w.ToUpper())));
 
-switch (choice)
+//Console.WriteLine("All 3 filters: " + string.Join(", ",words.Where(noX).Where(fourCharsOrMore).Where(sameasAverage))); //Print all that does not have x and has 4 or more characters and has same number of characters as the average
+
+//foreach (string item in words.Where(noX).Where(fourCharsOrMore).Where(sameasAverage))
+//{
+//    Console.WriteLine("All 3 filters: " + item); //Print each item in a new line
+//}
+
+//IEnumerable<string> query = from w in words
+//                            where !w.Contains("x")
+//                            where w.Length >= 4
+//                            where w.Length == avgLength
+//                            select w;
+
+//Console.WriteLine("All 3 filters: " + string.Join(", ", query));
+
+//--------------------------------------------------------------------------
+
+// Store filters in a list
+var filters = new List<Func<string, bool>> { noX, fourCharsOrMore, sameasAverage };
+
+// Display menu
+Console.WriteLine("The average length is: " + avgLength); //Print average length of words
+Console.WriteLine($"Array of strings : {string.Join(", ", words)}");
+Console.WriteLine("1. noX ");
+Console.WriteLine("2. fourCharsOrMore ");
+Console.WriteLine("3. sameasAverage ");
+Console.Write("\nChoice: ");
+
+// Read filter choice
+if (!int.TryParse(Console.ReadLine(), out int choice) || choice < 1 || choice > filters.Count)
 {
-    case "0":
-        Console.WriteLine("Choose display order: 1 for A-Z, 2 for Z-A, 3 for Reverse the array");
-        string orderChoice = Console.ReadLine();
-
-        if (orderChoice == "1")
-            Console.WriteLine(string.Join(", ", words.Where(fourCharsOrMore).OrderBy(w => w)));
-        else if (orderChoice == "2")
-            Console.WriteLine(string.Join(", ", words.Where(fourCharsOrMore).OrderByDescending(w => w)));
-        else if (orderChoice == "3")
-            Console.WriteLine(string.Join(", ", words.Where(fourCharsOrMore).Reverse()));
-
-        break;
-
-    case "1":
-        Console.WriteLine("Choose display order: 1 for A-Z, 2 for Z-A, 3 for Reverse the array");
-        orderChoice = Console.ReadLine();
-
-        if (orderChoice == "1")
-            Console.WriteLine(string.Join(", ", words.Where(noX).OrderBy(w => w)));
-        else if (orderChoice == "2")
-            Console.WriteLine(string.Join(", ", words.Where(noX).OrderByDescending(w => w)));
-        else if (orderChoice == "3")
-            Console.WriteLine(string.Join(", ", words.Where(noX).Reverse()));
-
-        break;
-
-    case "2":
-
-        Console.WriteLine("Choose display order: 1 for A-Z, 2 for Z-A, 3 for Reverse the array");
-        orderChoice = Console.ReadLine();
-
-        if (orderChoice == "1")
-            Console.WriteLine(string.Join(", ", words.Where(sameasAverage).OrderBy(w => w)));
-        else if (orderChoice == "2")
-            Console.WriteLine(string.Join(", ", words.Where(sameasAverage).OrderByDescending(w => w)));
-        else if (orderChoice == "3") 
-            Console.WriteLine(string.Join(", ", words.Where(sameasAverage).Reverse()));
-        
-        break;
-
-    default:
-        Console.WriteLine("Invalid choice. Please enter 1, 2, or 3.");
-        break;
+    Console.WriteLine("Choice invalid.");
+    return;
 }
 
-<<<<<<< HEAD
-/*
-=======
+var selectedFilter = filters[choice - 1];
 
->>>>>>> c733539eca0b63a01a4e64880685bf9ad0dfc52b
+// Ask for display order
+Console.WriteLine("Choose display order: 1 = A-Z, 2 = Z-A, 3 = Reverse the array");
+string orderChoice = Console.ReadLine();
+
+// Apply filter first
+IEnumerable<string> result = words.Where(selectedFilter);
+
+// Apply ordering
+result = orderChoice switch
+{
+    "1" => result.OrderBy(w => w),
+    "2" => result.OrderByDescending(w => w),
+    "3" => result.Reverse(),
+    _ => result
+};
+
+// Print result
+Console.WriteLine($"Résultat: {string.Join(", ", result)}");
+
+
+//---------------------------Action and Func------------------------------------------------
+/*
 void Greet()
 {
     Console.WriteLine("I am Action.");
 }
 Greet();
-<<<<<<< HEAD
-=======
 
->>>>>>> c733539eca0b63a01a4e64880685bf9ad0dfc52b
+Action greet = () => Console.WriteLine("I am Action!");
+greet();
+
+Console.WriteLine(" ");
+
 void Greet1(int a, bool b, string s)
 {
     Console.WriteLine($"{a} + {b} + {s}");
 }
 Greet1(1, true, "Meron");
 
-// Action without parameters
-Action greet = () => Console.WriteLine("Hello from Action!");
-greet(); // ✅ No arguments needed
-
-// Action with 3 parameters
 Action<int, bool, string> greet1 = (a, b, s) => Console.WriteLine($"{a} + {b} + {s}");
-greet1(1, true, "Essayas");
+greet1(1, true, "Meron");
+
+Console.WriteLine(" ");
+
+//--------------------------------------------------------------------------
 
 int f1()
 {
@@ -213,6 +119,12 @@ int f1()
 }
 
 Console.WriteLine(f1());
+
+Func<int> f4 = () => 123;
+Console.WriteLine(f4());
+
+Console.WriteLine(" ");
+
 int f2(int a)
 {
     return a * a;
@@ -220,26 +132,23 @@ int f2(int a)
 
 Console.WriteLine(f2(10));
 
+Func<int, int> f5 = a => a * a;
+Console.WriteLine(f5(10));
+
+Console.WriteLine(" ");
+
 int f3(int i, bool b, string s)
 {
     return i * 555;
 }
-
 Console.WriteLine(f3(3, true, "Tewelde"));
 
-// Func with no parameters, returns int
-Func<int> f4 = () => 123;
-Console.WriteLine(f4()); // 123
-
-// Func with 1 int parameter, returns int
-Func<int, int> f5 = a => a * a;
-Console.WriteLine(f5(10)); // 100
-
-// Func with 3 parameters (int, bool, string), returns int
 Func<int, bool, string, int> f6 = (i, b, s) => i * 555;
-Console.WriteLine(f6(3, true, "Tekleab")); // 2,775,000
-<<<<<<< HEAD
+Console.WriteLine(f6(3, true, "Tewelde"));
 
+Console.WriteLine(" ");
+
+//--------------------------------------------------------------------------
 
 int X2(int x)
 {
@@ -249,6 +158,9 @@ int X2(int x)
 Func<int, int> x3 = X2;
 Console.WriteLine(x3(5));
 
+Console.WriteLine(" ");
+
+//--------------------------------------------------------------------------
 
 int Superior(Func<int, int, int> x)
 {
@@ -263,10 +175,48 @@ int Sub(int a, int b)
 }
 
 Console.WriteLine(result);
-*/
+
+Console.WriteLine(" ");
+
+//--------------------------------------------------------------------------
 
 Func<int, Func<int, int>> myFunc = x => y => x + y;
 Console.WriteLine(myFunc(5)(3));
-=======
 */
->>>>>>> c733539eca0b63a01a4e64880685bf9ad0dfc52b
+//--------------------------------------------------------------------------
+
+
+//List<Person> people = new() {
+//    new Person ( name: "john", age: 25 ),
+//    new Person ( name: "mary", age: 30 ),
+//    new Person ( name: "jane", age: 20 ),
+//    new Person ( name: "bob", age: 35 )
+//};
+
+//List<Person> adults = people.Where(p => p.Name == "john").ToList();
+
+//adults.ForEach(n => Console.WriteLine(n.Name + " " + n.Age));
+//adults.ForEach(n => Console.WriteLine($"{n.Name} {n.Age}"));
+
+//Console.WriteLine(" ");
+
+//Console.WriteLine(string.Join(", ", adults.Select(n => n.Name + " " + n.Age)));
+//Console.WriteLine(string.Join(", ", adults.Select(n => $"{n.Name} {n.Age}")));
+
+//--------------------------------------------------------------------------
+
+//List<Coloc> coloc = new() {
+//    new Coloc() { Name = "William", Age = 30 }
+//};
+
+//coloc.ForEach(n => Console.WriteLine($"{n.Name} {n.Age}"));
+//Console.WriteLine(string.Join(", ", coloc.Select(n => n.Name + " " + n.Age)));
+
+////Class without constructor
+//class Coloc
+//{
+//    public string Name { get; set; }
+//    public int Age { get; set; }
+//}
+
+//--------------------------------------------------------------------------

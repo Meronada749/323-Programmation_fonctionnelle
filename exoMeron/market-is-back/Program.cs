57f3087d8812b6d3620d698520dc4b4b97e84d63
Meronada749 <meron.essayas@eduvaud.ch>
2025-09-17 15:38:19 +0200

    Update Program.cs

--- exoMeron/market-is-back/Program.cs
+++ exoMeron/market-is-back/Program.cs
@@ -8,85 +8,28 @@
 //Console.WriteLine(products.Count());
 //Console.WriteLine(products[0].Provider.Count());
 
+using System;
 using System.Security.Cryptography.X509Certificates;
 using System.Xml;
 
 List<Product> products = new()
 {
     new Product() { Location = "1", Provider = "Bornand", Name = "Pommes", Quantity = 20, Unit = "kg", Price = 6.90m },
-    new Product() { Location = "1", Provider = "Bornand", Name = "Poires", Quantity = 16, Unit = "kg", Price = 3.50m },
-    new Product() { Location = "1", Provider = "Bornand", Name = "Pastèques", Quantity = 14, Unit = "pièce", Price = 6.00m },
-    new Product() { Location = "1", Provider = "Bornand", Name = "Melons", Quantity = 5, Unit = "kg", Price = 7.00m },
     new Product() { Location = "2", Provider = "Dumont", Name = "Noix", Quantity = 20, Unit = "sac", Price = 8.60m },
-    new Product() { Location = "2", Provider = "Dumont", Name = "Raisin", Quantity = 6, Unit = "kg", Price = 5.60m },
-    new Product() { Location = "2", Provider = "Dumont", Name = "Pruneaux", Quantity = 13, Unit = "kg", Price = 8.10m },
-    new Product() { Location = "2", Provider = "Dumont", Name = "Myrtilles", Quantity = 12, Unit = "kg", Price = 8.90m },
-    new Product() { Location = "2", Provider = "Dumont", Name = "Groseilles", Quantity = 12, Unit = "kg", Price = 5.20m },
+    new Product() { Location = "2", Provider = "Dumont", Name = "Noix", Quantity = 20, Unit = "sac", Price = 8.0m },
     new Product() { Location = "3", Provider = "Vonlanthen", Name = "Pêches", Quantity = 8, Unit = "kg", Price = 8.70m },
-    new Product() { Location = "3", Provider = "Vonlanthen", Name = "Haricots", Quantity = 6, Unit = "kg", Price = 6.90m },
-    new Product() { Location = "3", Provider = "Vonlanthen", Name = "Courges", Quantity = 18, Unit = "pièce", Price = 4.30m },
-    new Product() { Location = "3", Provider = "Vonlanthen", Name = "Tomates", Quantity = 12, Unit = "kg", Price = 9.40m },
-    new Product() { Location = "3", Provider = "Vonlanthen", Name = "Pommes", Quantity = 20, Unit = "kg", Price = 3.90m },
     new Product() { Location = "4", Provider = "Barizzi", Name = "Poires", Quantity = 5, Unit = "kg", Price = 6.30m },
-    new Product() { Location = "4", Provider = "Barizzi", Name = "Pastèques", Quantity = 6, Unit = "pièce", Price = 2.50m },
-    new Product() { Location = "4", Provider = "Barizzi", Name = "Melons", Quantity = 14, Unit = "kg", Price = 4.20m },
-    new Product() { Location = "4", Provider = "Barizzi", Name = "Noix", Quantity = 20, Unit = "sac", Price = 7.50m },
-    new Product() { Location = "4", Provider = "Barizzi", Name = "Raisin", Quantity = 15, Unit = "kg", Price = 7.20m },
     new Product() { Location = "5", Provider = "Blanc", Name = "Pruneaux", Quantity = 5, Unit = "kg", Price = 9.00m },
-    new Product() { Location = "5", Provider = "Blanc", Name = "Myrtilles", Quantity = 18, Unit = "kg", Price = 5.60m },
-    new Product() { Location = "5", Provider = "Blanc", Name = "Groseilles", Quantity = 10, Unit = "kg", Price = 2.10m },
-    new Product() { Location = "5", Provider = "Blanc", Name = "Pêches", Quantity = 20, Unit = "kg", Price = 6.40m },
-    new Product() { Location = "5", Provider = "Blanc", Name = "Haricots", Quantity = 9, Unit = "kg", Price = 2.90m },
     new Product() { Location = "6", Provider = "Repond", Name = "Courges", Quantity = 12, Unit = "pièce", Price = 7.40m },
-    new Product() { Location = "6", Provider = "Repond", Name = "Tomates", Quantity = 12, Unit = "kg", Price = 4.20m },
-    new Product() { Location = "6", Provider = "Repond", Name = "Pommes", Quantity = 15, Unit = "kg", Price = 6.50m },
-    new Product() { Location = "6", Provider = "Repond", Name = "Poires", Quantity = 18, Unit = "kg", Price = 2.40m },
-    new Product() { Location = "6", Provider = "Repond", Name = "Pastèques", Quantity = 7, Unit = "pièce", Price = 5.70m },
     new Product() { Location = "7", Provider = "Mancini", Name = "Pêches", Quantity = 10, Unit = "kg", Price = 2.90m },
-    new Product() { Location = "7", Provider = "Mancini", Name = "Haricots", Quantity = 11, Unit = "kg", Price = 6.70m },
-    new Product() { Location = "7", Provider = "Mancini", Name = "Courges", Quantity = 10, Unit = "pièce", Price = 6.40m },
-    new Product() { Location = "7", Provider = "Mancini", Name = "Tomates", Quantity = 13, Unit = "kg", Price = 1.50m },
-    new Product() { Location = "7", Provider = "Mancini", Name = "Pommes", Quantity = 14, Unit = "kg", Price = 7.00m },
     new Product() { Location = "8", Provider = "Favre", Name = "Poires", Quantity = 5, Unit = "kg", Price = 8.40m },
-    new Product() { Location = "8", Provider = "Favre", Name = "Pastèques", Quantity = 5, Unit = "pièce", Price = 1.70m },
-    new Product() { Location = "8", Provider = "Favre", Name = "Haricots", Quantity = 5, Unit = "kg", Price = 3.00m },
-    new Product() { Location = "8", Provider = "Favre", Name = "Courges", Quantity = 17, Unit = "pièce", Price = 2.00m },
-    new Product() { Location = "8", Provider = "Favre", Name = "Tomates", Quantity = 9, Unit = "kg", Price = 5.20m },
     new Product() { Location = "9", Provider = "Bovay", Name = "Pommes", Quantity = 13, Unit = "kg", Price = 7.70m },
-    new Product() { Location = "9", Provider = "Bovay", Name = "Poires", Quantity = 5, Unit = "kg", Price = 3.80m },
-    new Product() { Location = "9", Provider = "Bovay", Name = "Pastèques", Quantity = 20, Unit = "pièce", Price = 2.10m },
-    new Product() { Location = "9", Provider = "Bovay", Name = "Melons", Quantity = 20, Unit = "kg", Price = 6.40m },
-    new Product() { Location = "9", Provider = "Bovay", Name = "Noix", Quantity = 13, Unit = "sac", Price = 8.80m },
     new Product() { Location = "10", Provider = "Cherix", Name = "Raisin", Quantity = 8, Unit = "kg", Price = 7.10m },
-    new Product() { Location = "10", Provider = "Cherix", Name = "Pruneaux", Quantity = 19, Unit = "kg", Price = 7.90m },
-    new Product() { Location = "10", Provider = "Cherix", Name = "Myrtilles", Quantity = 9, Unit = "kg", Price = 4.20m },
-    new Product() { Location = "10", Provider = "Cherix", Name = "Groseilles", Quantity = 10, Unit = "kg", Price = 4.40m },
-    new Product() { Location = "10", Provider = "Cherix", Name = "Pêches", Quantity = 9, Unit = "kg", Price = 4.40m },
     new Product() { Location = "11", Provider = "Beaud", Name = "Haricots", Quantity = 19, Unit = "kg", Price = 8.40m },
-    new Product() { Location = "11", Provider = "Beaud", Name = "Courges", Quantity = 16, Unit = "pièce", Price = 8.70m },
-    new Product() { Location = "11", Provider = "Beaud", Name = "Tomates", Quantity = 18, Unit = "kg", Price = 5.30m },
-    new Product() { Location = "11", Provider = "Beaud", Name = "Pommes", Quantity = 8, Unit = "kg", Price = 7.30m },
-    new Product() { Location = "11", Provider = "Beaud", Name = "Poires", Quantity = 13, Unit = "kg", Price = 9.20m },
     new Product() { Location = "12", Provider = "Corbaz", Name = "Pastèques", Quantity = 15, Unit = "pièce", Price = 7.40m },
-    new Product() { Location = "12", Provider = "Corbaz", Name = "Melons", Quantity = 12, Unit = "kg", Price = 1.60m },
-    new Product() { Location = "12", Provider = "Corbaz", Name = "Noix", Quantity = 11, Unit = "sac", Price = 7.50m },
-    new Product() { Location = "12", Provider = "Corbaz", Name = "Raisin", Quantity = 16, Unit = "kg", Price = 4.50m },
-    new Product() { Location = "12", Provider = "Corbaz", Name = "Pruneaux", Quantity = 20, Unit = "kg", Price = 3.30m },
     new Product() { Location = "13", Provider = "Amaudruz", Name = "Myrtilles", Quantity = 18, Unit = "kg", Price = 5.70m },
-    new Product() { Location = "13", Provider = "Amaudruz", Name = "Groseilles", Quantity = 19, Unit = "kg", Price = 8.00m },
-    new Product() { Location = "13", Provider = "Amaudruz", Name = "Pêches", Quantity = 12, Unit = "kg", Price = 5.50m },
-    new Product() { Location = "13", Provider = "Amaudruz", Name = "Haricots", Quantity = 13, Unit = "kg", Price = 5.20m },
-    new Product() { Location = "13", Provider = "Amaudruz", Name = "Courges", Quantity = 7, Unit = "pièce", Price = 9.60m },
     new Product() { Location = "14", Provider = "Bühlmann", Name = "Tomates", Quantity = 12, Unit = "kg", Price = 7.70m },
-    new Product() { Location = "14", Provider = "Bühlmann", Name = "Pommes", Quantity = 17, Unit = "kg", Price = 1.90m },
-    new Product() { Location = "14", Provider = "Bühlmann", Name = "Poires", Quantity = 7, Unit = "kg", Price = 3.00m },
-    new Product() { Location = "14", Provider = "Bühlmann", Name = "Pastèques", Quantity = 11, Unit = "pièce", Price = 6.90m },
-    new Product() { Location = "14", Provider = "Bühlmann", Name = "Melons", Quantity = 7, Unit = "kg", Price = 4.70m },
     new Product() { Location = "15", Provider = "Crizzi", Name = "Noix", Quantity = 10, Unit = "sac", Price = 1.60m },
-    new Product() { Location = "15", Provider = "Crizzi", Name = "Raisin", Quantity = 17, Unit = "kg", Price = 7.80m },
-    new Product() { Location = "15", Provider = "Crizzi", Name = "Pruneaux", Quantity = 18, Unit = "kg", Price = 9.00m },
-    new Product() { Location = "15", Provider = "Crizzi", Name = "Myrtilles", Quantity = 12, Unit = "kg", Price = 3.00m },
-    new Product() { Location = "15", Provider = "Crizzi", Name = "Groseilles", Quantity = 12, Unit = "kg", Price = 3.50m }
 };
 
 Dictionary<string, string> Dict = new()
@@ -125,9 +68,111 @@ List<string> transformednames = products.Select(p => p.Provider.Substring(0, 3) 
 List<string> translatednames = products.Select(p => Dict[p.Name]).ToList();
 List<decimal> chiffreaffaires = products.Select(p => p.Quantity * p.Price).ToList();
 
-Enumerable.Range(0, products.Count()).ToList().ForEach(x=> Console.WriteLine("seller,product,ca \n" + string.Join(",", $"{x}, {translatednames}, {chiffreaffaires}")));
+//IEnumerable<string> print = Enumerable.Range(0, products.Count)
+//    .Select(i => string.Join(" | ", transformednames[i], translatednames[i], chiffreaffaires[i]));
+
+//// Add header + join everything
+//string result = "Seller,Product,CA" + Environment.NewLine + string.Join(Environment.NewLine, print);
+
+//Console.WriteLine(result);
 
 //string allExport = string.Join("\n", products.Select(p => (transformNames(p), translate(p), chiffreAffaire(p))));
 //File.WriteAllText("productExport.csv", $"Seller, Product,CA\n{allExport}");
 
+//--------------------------------------------SELLECT--------------------------------------------------------------------------------------
 
+IEnumerable<string> names = products.Select(x => x.Name);
+//Console.WriteLine(string.Join(", ", names)); //print horizontally
+//names.ToList().ForEach(Console.WriteLine); //print vertically
+
+IEnumerable<int> addition = products.Select(x => x.Quantity + (int)x.Price);
+//Console.WriteLine(string.Join(", ", addition)); //print horizontally
+//addition.ToList().ForEach(Console.WriteLine); //print vertically
+
+IEnumerable<(string Provider, int Quantity)> adults = products.Select(x => (x.Provider, x.Quantity)).Where(tuple => tuple.Item2 >= 18);
+//Console.WriteLine(adults.First().Item1); //print horizontally
+//adults.Take(1).ToList().ForEach(item => Console.WriteLine($"{item.Provider}"));
+
+//--------------------------------------------TUPLES--------------------------------------------------------------------------------------
+var f = (1, 2, 3);
+//Console.WriteLine(f.Item1);
+//Console.WriteLine(f.Item2);
+//Console.WriteLine(f.Item3);
+
+(int first, int second, int third) ff = (first: 1, second: 2, third: 3);
+//Console.WriteLine(ff.first);
+//Console.WriteLine(ff.second);
+//Console.WriteLine(ff.third);
+
+//--------------------------------------------ANONYMOUS TYPES--------------------------------------------------------------------------------------
+
+var fff = new { first = 1, second = 2, third = 3 };
+//Console.WriteLine(fff.first);
+//Console.WriteLine(fff.second);
+
+//var projections = products.Select(p => new
+//{
+//    p.Provider,
+//    p.Name
+//});
+
+//foreach (var x in projections)
+//{
+//    Console.WriteLine($"{x.Provider} - {x.Name}");
+//}
+
+
+//--------------------------------------------GROUP BY--------------------------------------------------------------------------------------
+var productsByProvider = products.GroupBy(p => p.Provider);
+
+//foreach (var group in productsByProvider)
+//{
+//    Console.WriteLine($"Provider: {group.Key}");
+//    foreach (var p in group)
+//    {
+//        Console.WriteLine($"  {p.Name} - {p.Quantity} x {p.Price} CHF");
+//    }
+//}
+
+//--------------------------------------------TO LIST--------------------------------------------------------------------------------------
+//Converts an IEnumerable<string> to a List<string>.
+//var productNames = products
+//    .Select(p => p.Name)
+//    .ToList();
+
+//productNames.ForEach(Console.WriteLine);
+
+//--------------------------------------------TO DICTIONARY--------------------------------------------------------------------------------------
+//var productDict = products.DistinctBy(p => p.Name)
+//    .ToDictionary(
+//        p => p.Provider,  // key
+//        p => p.Price  // value
+//    );
+
+//Console.WriteLine(productDict["Dumont"]); // prints the price of "Pommes"
+
+//--------------------------------------------TO ARRAY--------------------------------------------------------------------------------------
+//var productPrices = products
+//    .Select(p => p.Price)
+//    .ToArray();
+
+//foreach (var price in productPrices)
+//{
+//    Console.WriteLine(price);
+//}
+
+Dictionary<string, Product> dico = new()
+{
+    { "Pommes", new Product { Name = "Pommes", Quantity = 20, Price = 6.9m } },
+    { "Poires", new Product { Name = "Poires", Quantity = 16, Price = 3.5m } },
+    { "Pastèques", new Product { Name = "Pastèques", Quantity = 14, Price = 6.0m } },
+    { "Melons", new Product { Name = "Melons", Quantity = 5, Price = 7.0m } }
+};
+
+//Dictionary
+Product toto = dico["Pommes"];
+Console.WriteLine(toto.Price);
+
+//List
+Product tota = products.Where(p => p.Quantity == 5).First();
+Console.WriteLine(tota.Name);
